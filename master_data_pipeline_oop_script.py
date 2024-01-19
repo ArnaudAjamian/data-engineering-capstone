4c9a48d6bbddbce577d184473f85657a3e4c3441
Arnaud Ajamian <arnaud.ajamian@gmail.com>
2024-01-19 15:03:02 -0500

    added a CensusMigration class

--- master_data_pipeline_oop_script.py
+++ master_data_pipeline_oop_script.py
@@ -8,10 +8,14 @@ from google.cloud import bigquery
 import concurrent.futures
 
 
-# Modules for HomeRentalInsurance class
+# Modules for HomeRentalInsurance and CensusMigration class
 import requests
 from bs4 import BeautifulSoup
 
+
+# Modules for CensusMigration class
+import urllib.parse
+import os
 
 
 class NOAA:
@@ -199,3 +203,139 @@ df_cleaned = insurance_instance.create_dataframe()
 
 output_file_path = f"{insurance_file_path}/insurance_by_year_and_state.csv"
 insurance_instance.display_and_save_data(df_cleaned, output_file_path)
+
+
+
+
+
+
+class CensusMigration:
+
+    def __init__(self):
+        self.census_raw_files_folder = "State to State Migration Flows - Raw Excel Data"
+        self.census_cleaned_files_folder = "State to State Migration Flows - Cleaned Excel Data"
+
+    def download_raw_data(self, url):
+        
+        # Create a new folder containing all of the Excel files from 'url'
+        if not os.path.exists(self.census_raw_files_folder):
+            os.makedirs(self.census_raw_files_folder)
+
+        # Send a GET request to the URL and parse
+        # the HTML content using BeautifulSoup
+        html_text = requests.get(url).text
+        soup = BeautifulSoup(html_text, 'html.parser')
+
+        # Find all 'a' tags containing links to Excel files
+        excel_links = soup.select('a[href$=".xls"]')
+
+        for link in excel_links:
+
+            """
+            For each link for which an Excel file has been found (ending in .xls),
+            generate a complete URL by joining the base URL with the one found
+            following the 'href' attribute from the 'a' tag (represented as 'file_url'). 
+            """
+
+            file_url = urllib.parse.urljoin(url, link['href'])
+            file_name = os.path.join(self.census_raw_files_folder, os.path.basename(file_url).lower())
+
+            print(f"\nDownloading {file_url}...")
+
+            # Send a GET request to download the file
+            file_response = requests.get(file_url)
+
+            # Save the file to 'census_raw_files_folder'
+            with open(file_name, 'wb') as file:
+                file.write(file_response.content)
+
+            print(f"File saved as {file_name}")
+
+
+    def process_excel_file(self, file_path):
+        
+        # Read in the Excel file and convert it to a dataframe.
+        # Set the column headers equal to rows 6 and 7.
+        df = pd.read_excel(file_path, header=[6, 7])
+
+
+        """
+        Over time, the format of the 'State to State Migration' tables has changed.
+        From 2010 onward, additional columns were prepended to the original tables
+            which shifted the target data:
+            
+            ie. 'Alabama' was presented in column B from 2005 - 2009
+                'Alabama' is presented in column J from 2010 onward
+
+        Since the target data, interstate migration figures, is tabulated from the
+            'Alabama' column onward, we must first capture its column index.  
+        """
+        alabama_index = np.where(df.columns.get_loc('Alabama'))[0][0]
+
+        # Slice the dataframe to exclude those columns which do not represent
+        # state to state migration figures (applicable for all tables from 2010 onward)
+        df = df.iloc[:, [0] + list(range(alabama_index, df.shape[1]))]
+
+        # Delete any blank rows from the dataframe since the states listed in column A
+        # are generally partitioned in groups of 5.
+        df = df.dropna(axis=0, how='all')
+
+        """
+        In the original Excel file, there are multi-index column headers.
+        - Level 1 = State Name (row 7)
+        - Level 2 = Estimate and MOE (Margin of Error) (row 8)
+
+        Below, the 1st level (State Name) is converted into a separate column.
+        """
+        df = df.set_index(df.columns[0]) \
+                .stack(level=0) \
+                .reset_index() \
+                .rename({df.columns[0]: 'Moved To: State',
+                        'level_1': 'Moved From: State'}, axis=1)
+
+        # Filter out any rows where the 'Moved To' and 'Moved From' states are equal
+        # since we only want to capture interstate migration flows.
+        df = df[(df['Moved To: State'] != df['Moved From: State'])
+                & (~df['Moved To: State'].str.contains("United States", na=False))]
+
+        df.drop(['MOE.1', 'Estimate.1'], axis=1, inplace=True, errors='ignore')
+        df = df.dropna(axis=0, how='any')
+
+        df.reset_index(drop=True, inplace=True)
+
+        return df
+
+
+
+    def process_raw_data(self):
+
+        # Define the folder containing the Excel files
+        # and get a list of Excel files in the folder
+        folder_path = f"{self.census_raw_files_folder}"
+        file_list = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.xls')]
+
+        if not os.path.exists(self.census_cleaned_files_folder):
+            os.makedirs(self.census_cleaned_files_folder)
+
+        for file in file_list:
+            if not file.endswith("state_migration_flows_tables.xls"):
+
+                result_df = self.process_excel_file(file)
+
+                # Retrieve the original file name without extension and replace "table" with "dataframe"
+                original_file_name = os.path.splitext(os.path.basename(file))[0]
+                new_file_name = original_file_name.replace("table", "dataframe") + ".xlsx"
+
+                # Define the path which will store the results of the 'process_excel_file' function
+                processed_file_path = os.path.join(self.census_cleaned_files_folder, new_file_name)
+
+                # Save the processed DataFrame to the designated folder
+                result_df.to_excel(processed_file_path, index=False)
+                print(f"\nSaving the processed raw data files...")
+                print(f"The '{new_file_name}' file has been saved to the '{self.census_cleaned_files_folder}' folder.")
+
+
+
+census_migration = CensusMigration()
+census_migration.download_raw_data('https://www.census.gov/data/tables/time-series/demo/geographic-mobility/state-to-state-migration.html')
+census_migration.process_raw_data()
